6c7754938f71b1bb5e7aa2a0f2dcb491d6d2ec7c
alt <95161279+alt3ri@users.noreply.github.com>
2022-10-15 16:42:42 +0700

    Adding config yay

---
+++ src/main/java/thorny/grasscutters/AttackModifier/utils/Config.java
@@ -0,0 +1,75 @@
+package thorny.grasscutters.AttackModifier.utils;
+
+public final class Config {
+    public characters ayakaIds = new characters();
+    public characters jeanIds = new characters();
+    public characters travelerMaleNoElementIds = new characters();
+    public characters lisaIds = new characters();
+    public characters travelerFemaleNoElementIds = new characters();
+    public characters barbaraIds = new characters();
+    public characters kaeyaIds = new characters();
+    public characters dilucIds = new characters();
+    public characters razorIds = new characters();
+    public characters amberIds = new characters();
+    public characters ventiIds = new characters();
+    public characters xianglingIds = new characters();
+    public characters beidouIds = new characters();
+    public characters xingqiuIds = new characters();
+    public characters xiaoIds = new characters();
+    public characters ningguangIds = new characters();
+    public characters kleeIds = new characters();
+    public characters zhongliIds = new characters();
+    public characters fischlIds = new characters();
+    public characters bennettIds = new characters();
+    public characters tartagliaIds = new characters();
+    public characters noelleIds = new characters();
+    public characters qiqiIds = new characters();
+    public characters chongyunIds = new characters();
+    public characters ganyuIds = new characters();
+    public characters albedoIds = new characters();
+    public characters dionaIds = new characters();
+    public characters monaIds = new characters();
+    public characters keqingIds = new characters();
+    public characters sucroseIds = new characters();
+    public characters xinyanIds = new characters();
+    public characters rosariaIds = new characters();
+    public characters hutaoIds = new characters();
+    public characters kazuhaIds = new characters();
+    public characters yanfeiIds = new characters();
+    public characters yoimiyaIds = new characters();
+    public characters thomaIds = new characters();
+    public characters eulaIds = new characters();
+    public characters raidenshogunIds = new characters();
+    public characters sayuIds = new characters();
+    public characters kokomiIds = new characters();
+    public characters gorouIds = new characters();
+    public characters kujousaraIds = new characters();
+    public characters ittoIds = new characters();
+    public characters yaemikoIds = new characters();
+    public characters heizouIds = new characters();
+    public characters yelanIds = new characters();
+    public characters aloyIds = new characters();
+    public characters shenheIds = new characters();
+    public characters yunjinIds = new characters();
+    public characters kukishinobuIds = new characters();
+    public characters ayatoIds = new characters();
+    public characters colleiIds = new characters();
+    public characters doriIds = new characters();
+    public characters tighnariIds = new characters();
+    public characters nilouIds = new characters();
+    public characters cynoIds = new characters();
+    public characters candaceIds = new characters();
+    public characters nahidaIds = new characters();
+    public characters laylaIds = new characters();
+
+    public static class characters {
+
+        public skillIds skill = new skillIds();
+
+        public static class skillIds {
+            public int normalAtk = 0;
+            public int elementalSkill = 0;
+            public int elementalBurst = 0;
+        }
+    }
+}
